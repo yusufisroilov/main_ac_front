f530239f5db96bc8e9937baeda5cd28f6780091f
Fazliddin-SN <yumasoftuz2@gmail.com>
2025-07-31 12:04:55 +0500

    test commit

--- src/app/uzb_staff/boxes-list/boxes-list.component.ts
+++ src/app/uzb_staff/boxes-list/boxes-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-boxes-list',
-  templateUrl: './boxes-list.component.html',
-  styleUrls: ['./boxes-list.component.css']
+  selector: "app-boxes-list",
+  templateUrl: "./boxes-list.component.html",
+  styleUrls: ["./boxes-list.component.css"],
 })
 export class BoxesListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+// commit
